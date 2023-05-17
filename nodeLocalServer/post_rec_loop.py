41ce8ecf44b01b1b3226d45c9d9d2f371588f3be
mnlsvt <manolis277@gmail.com>
2023-05-18 01:08:35 +0300

    made the py loop better

--- nodeLocalServer/post_rec_loop.py
+++ nodeLocalServer/post_rec_loop.py
@@ -3,37 +3,38 @@ import time
 
 #url = "http://localhost:3000/sensor-data"
 url = "http://mnlsvtserver.ddns.net:3000/sensor-data"
-for i in range(10):
-        if (i==1 or i==3 or i==5 or i==7 or i==8 or i==10):
-                data = {"storeId" : 1,
-                        "tableId" : 1,
-                        "isFree" : "yes"}
-        else:
-                data = {"storeId" : 1,
-                        "tableId" : 1,
-                        "isFree" : "no"}
-        r = requests.post(url,data=data)
+while True:
+        for i in range(10):
+                if (i==1 or i==3 or i==5 or i==7 or i==8 or i==10):
+                        data = {"storeId" : 1,
+                                "tableId" : 1,
+                                "isFree" : "yes"}
+                else:
+                        data = {"storeId" : 1,
+                                "tableId" : 1,
+                                "isFree" : "no"}
+                r = requests.post(url,data=data)
 
-        if (i==1 or i==3 or i==5 or i==7 or i==8 or i==10):
-                data = {"storeId" : 2,
-                        "tableId" : 1,
-                        "isFree" : "yes"}
-        else:
-                data = {"storeId" : 2,
-                        "tableId" : 1,
-                        "isFree" : "no"}
-        r = requests.post(url,data=data)
+                if (i==1 or i==3 or i==5 or i==7 or i==8 or i==10):
+                        data = {"storeId" : 2,
+                                "tableId" : 1,
+                                "isFree" : "yes"}
+                else:
+                        data = {"storeId" : 2,
+                                "tableId" : 1,
+                                "isFree" : "no"}
+                r = requests.post(url,data=data)
 
-        if (i==1 or i==3 or i==5 or i==7 or i==8 or i==10):
-                data = {"storeId" : 3,
-                        "tableId" : 1,
-                        "isFree" : "yes"}
-        else:
-                data = {"storeId" : 3,
-                        "tableId" : 1,
-                        "isFree" : "no"}
-        r = requests.post(url,data=data)
+                if (i==1 or i==3 or i==5 or i==7 or i==8 or i==10):
+                        data = {"storeId" : 3,
+                                "tableId" : 1,
+                                "isFree" : "yes"}
+                else:
+                        data = {"storeId" : 3,
+                                "tableId" : 1,
+                                "isFree" : "no"}
+                r = requests.post(url,data=data)
 
 
-        time.sleep(5)
+                time.sleep(5)
 print(r.text)
